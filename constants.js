c8e99baabd3f56707b915caf1f4229d55c821d81
Leonhelm <qwiray@gmail.com>
2022-09-06 10:18:35 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const TOKEN = Deno.env.get("TOKEN");
 export const USER_NAME = Deno.env.get("USER_NAME");
 
 export const GAME_URL = "https://xcraft.ru";
-export const MAX_CAPITAL_RESOURCES = 3_000_000;
+export const MAX_CAPITAL_RESOURCES = 1_000_000;
 export const MAX_COLONY_RESOURCES = 100_000;
 export const MAX_EXPEDITIONS = +new Date() > +new Date('Jul 14, 2022 23:59:59') ? 3 : 4;
 
