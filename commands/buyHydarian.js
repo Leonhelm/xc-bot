f0db2c7f1d25f2141ae61fa4b0e189d5ecd5cd81
Demkin Vitalii <vitalii.demkin@factory5.ai>
2023-05-24 19:25:57 +0300

    fix buyHydarian

--- commands/buyHydarian.js
+++ commands/buyHydarian.js
@@ -8,10 +8,6 @@ export const buyHydarian = async (planet) => {
         const dendrariumPage = await makeRequestText(`/dendrarium/?f[0][key]=hydarian&f[0][as]=sell&f[0][from]=0&f[0][to]=0&pagination=${pagination}`);
         const rawOffers = dendrariumPage.split('<span class="short" data-help="Кристаллы Хайдариан:').slice(1);
 
-        if (pagination > 1 && rawOffers.length < 50) {
-            break;
-        }
-
         offers.push(...rawOffers.map(rawOffer => {
             const isOverflow = rawOffer.includes('overflow">');
 
@@ -23,7 +19,7 @@ export const buyHydarian = async (planet) => {
             const commission = +rawOffer.split("<span class='hydarian res-icon'>").at(1).split("</span>").at(0).trim();
             const benefit = hydarian / commission;
 
-            if (isNaN(benefit) || benefit < 5) {
+            if (isNaN(benefit) || benefit < 3) {
                 return;
             }
 
@@ -35,6 +31,10 @@ export const buyHydarian = async (planet) => {
 
             return { benefit, token };
         }).filter(Boolean));
+
+        if (rawOffers.length < 50) {
+            break;
+        }
     }
 
     if (!offers.length) {
