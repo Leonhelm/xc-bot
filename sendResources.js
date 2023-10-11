88a258f86d81b70e10033cb398e8cd8e9a51ae1c
Demkin Vitalii <vitalii.demkin@factory5.ai>
2023-10-11 10:13:46 +0300

    fix

--- sendResources.js
+++ sendResources.js
@@ -2,14 +2,18 @@ import { sendResourcesToCapital } from "./commands/sendResourcesToCapital.js";
 import { takingActionsOnPlanets } from "./commands/takingActionsOnPlanets.js";
 import { collectionResources } from "./commands/collectionResources.js";
 
+const reserve = 5_000;
+
 await takingActionsOnPlanets(
   async (planet) => {
-    const { type, id } = planet;
+    const { type, id, metal, crystal, deuterium } = planet;
 
     await collectionResources(id);
 
     if (type === "colony") {
-      await sendResourcesToCapital(planet);
+      if (metal > reserve || crystal > reserve || deuterium > reserve) {
+        await sendResourcesToCapital(planet);
+      }
     }
   },
   (planets) => planets.reverse()
