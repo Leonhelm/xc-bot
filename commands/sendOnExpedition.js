d50b598ceb0540ad1b799d77f88c06e6a4a48b4e
Leonhelm <qwiray@pm.me>
2024-10-17 09:47:43 +0300

    fix

--- commands/sendOnExpedition.js
+++ commands/sendOnExpedition.js
@@ -1,5 +1,5 @@
 import { createFleet } from "./createFleet.js";
-import { CAPITAL, QUEEN, MAX_EXPEDITIONS } from "../constants.js";
+import { QUEEN, MAX_EXPEDITIONS } from "../constants.js";
 import { makeRequestJson } from "../utils/makeRequest.js";
 import { getMyFleetInFly } from "../utils/fleetInFly.js";
 
@@ -14,7 +14,7 @@ export const sendOnExpedition = async (planet, page) => {
 
     if (queenInReserveCount > 0) {
       const response = await makeRequestJson("/fleet/send/", {
-        body: `ship%5B${QUEEN.id}%5D=1&target_user=&method=get&use_portal=false&metal=0&crystal=0&deuterium=0&galaxy=${CAPITAL.galaxy}&system=${CAPITAL.system}&planet=10&planettype=1&planetId=0&mission=15&holding=24&hyd=0&speed=100&fleet_group=0&fid=0&fleet_resource_priority=0&rec-auto-return=1&aggression=0&battle_begin_alarm=0&count=0&silent=0`,
+        body: `ship%5B${QUEEN.id}%5D=1&target_user=&method=get&use_portal=false&metal=0&crystal=0&deuterium=0&galaxy=${planet.galaxy}&system=${planet.system}&planet=10&planettype=1&planetId=0&mission=15&holding=24&hyd=0&speed=100&fleet_group=0&fid=0&fleet_resource_priority=0&rec-auto-return=1&aggression=0&battle_begin_alarm=0&count=0&silent=0`,
         method: "POST",
       });
 
